eb72b7be5733b0a3ebf8b9b89aca82f59a1bcc73
gustavop1903 <gustavogp1903@hotmail.com>
2022-09-11 13:09:45 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
-import NotificationButton from "./components/NotificationButton"
 import Header from "./components/Header"
 import SalesCard from "./components/SalesCard"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import NotificationButton from "./components/NotificationButton";
+
 
 function App() {
 
   return (
     <>
+    <ToastContainer />
       <Header></Header>
       <main>
         <section id="sales">
